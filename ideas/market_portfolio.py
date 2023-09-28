941ea81a00a41534028b8094395605e443def58a
Cfir <59117516+Cphyr@users.noreply.github.com>
2023-09-28 20:24:27 +0300

    finished market portfolio

--- ideas/market_portfolio.py
+++ ideas/market_portfolio.py
@@ -15,16 +15,43 @@ class MarketPortfolio(Portfolio):
     def __init__(self, weights=None) -> None:
         super().__init__(weights)
 
-    def train(self, tick2df: dict) -> None:
-        # TODO: implement
+    def train(self, train_data: pd.DataFrame) -> None:
+        self.train_data = train_data
+        self.last_alocation = self._get_allocation_based_on_market_value(
+            self.train_data
+        )
 
         pass
 
     def get_portfolio(self, train_data: pd.DataFrame) -> np.array:
-        # TODO: implement
-        pass
+        self.last_alocation = self._get_allocation_based_on_market_value(
+            self.train_data
+        )
+        return self.last_alocation
 
     def get_prices(self, train_data: pd.DataFrame) -> np.array:
         # TODO: implement
         print("idk what to do here")
         pass
+
+    def _get_allocation_based_on_market_value(df):
+        """
+        Calculates allocation based on market value.
+
+        Args:
+            df: A Pandas DataFrame containing the market values of the assets.
+
+        Returns:
+            A Pandas Series containing the allocation of each asset.
+        """
+        market_values = df["Adj Close"]
+
+        # take last index
+        market_values = market_values.iloc[-1]
+        # Calculate the total market value.
+        total_market_value = market_values.sum()
+
+        # Calculate the allocation of each asset.
+        allocation = market_values / total_market_value
+
+        return allocation
