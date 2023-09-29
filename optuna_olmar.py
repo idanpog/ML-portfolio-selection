4859c2518158687ca334c06433da03439a2d8ec7
Cfir <59117516+Cphyr@users.noreply.github.com>
2023-09-29 10:14:16 +0300

    optuna

---
+++ optuna_olmar.py
@@ -0,0 +1,19 @@
+"""
+Author: Cfir Hadar
+
+Description:
+Parameters optimization for OLMAR algorithm using Optuna.
+Parameters are: eps, w.
+
+"""
+
+import optuna
+import cross_validate
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
