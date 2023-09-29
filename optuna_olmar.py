acb93cf6ffdd805f513b8298aa13a14586879de8
Cfir <59117516+Cphyr@users.noreply.github.com>
2023-09-29 10:22:03 +0300

    optuna finished

--- optuna_olmar.py
+++ optuna_olmar.py
@@ -12,7 +12,10 @@ import cross_validate
 
 
 def main():
-    pass
+    # Maximize cross_validation.run function
+    study = optuna.create_study(direction="maximize")
+    study.optimize(cross_validate.run, n_trials=1000)
+    print(study.best_trial)
 
 
 if __name__ == "__main__":
